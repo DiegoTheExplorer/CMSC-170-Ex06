c4e3fd7cc3bd34f4c83b27bcf8413e5e6bbf80fc
Diego Villamil <dmvillamil@up.edu.ph>
2023-11-11 14:53:23 +0800

    All weights correct for first iteration

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ for line in fp: #Read the feature vectors and expected output from input.txt
   numX = len(line) - 1
   line.insert(len(line) - 1, bias)
 
-  for ind in range(0,(numX + 2)): #Add 0s for weights, perceptron value (a) and classification (y)
+  for ind in range(0,(numX + 3)): #Add 0s for weights, perceptron value (a) and classification (y)
     line.insert(len(line) - 1, float(0))
   pTable.append(line)
 fp.close()
@@ -35,14 +35,16 @@ y = ln - 2
 z = ln - 1
 for row in range(0,len(pTable) - 1):
   #Compute perceptron value a
-  for i in range(0,(numX)):
-    pTable[row][a] = pTable[row][a] + (pTable[row][i] * pTable[row][i + numX])
+  for i in range(0,(numX + 1)):
+    pTable[row][a] = pTable[row][a] + (pTable[row][i] * pTable[row][i + numX + 1])
 
   #Determine classification
-  pTable[row][y] = 1 if pTable[row][a] > th else 0
+  pTable[row][y] = 1 if pTable[row][a] >= th else 0
 
   #Adjust weights
   for i in range(0,(numX + 1)):
-    pTable[row + 1][i + numX] = pTable[row][i + numX] + (lr * pTable[row][i] * (pTable[row][z] - pTable[row][y]))
+    currW = pTable[row][i + numX + 1]
+    newW = currW + (lr * pTable[row][i] * (pTable[row][z] - pTable[row][y]))
+    pTable[row + 1][i + numX + 1] = newW
   print(pTable[row])
 
