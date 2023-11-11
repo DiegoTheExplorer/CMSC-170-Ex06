6f59215e0950b3cfd8f361ec0e388d271f7cf883
Diego Villamil <dmvillamil@up.edu.ph>
2023-11-11 13:36:04 +0800

    Works for 1 iteration but calculation is wrong

--- main.py
+++ main.py
@@ -18,25 +18,31 @@ iterations = []
 for line in fp: #Read the feature vectors and expected output from input.txt
   line = line.split(" ")
   line = list(map(float,line))
-  numX = len(line)
+  numX = len(line) - 1
   line.insert(len(line) - 1, bias)
 
   for ind in range(0,(numX + 2)): #Add 0s for weights, perceptron value (a) and classification (y)
     line.insert(len(line) - 1, float(0))
   pTable.append(line)
 fp.close()
+pTable.append([])
+for ind in range(0,len(pTable[0])):
+  pTable[len(pTable) - 1].append(0)
 
 ln = len(pTable[0])
 a = ln - 3
 y = ln - 2
 z = ln - 1
-for row in range(0,len(pTable)):
+for row in range(0,len(pTable) - 1):
   #Compute perceptron value a
-  for i in range(0,(numX + 1)):
-    pTable[row][a] = pTable[row][a] + (pTable[row][i] * pTable[row][numX + 1])
+  for i in range(0,(numX)):
+    pTable[row][a] = pTable[row][a] + (pTable[row][i] * pTable[row][i + numX])
 
   #Determine classification
-  pTable[row][y] = 0 if pTable[row][a] >= th else 0
+  pTable[row][y] = 1 if pTable[row][a] > th else 0
 
   #Adjust weights
+  for i in range(0,(numX + 1)):
+    pTable[row + 1][i + numX] = pTable[row][i + numX] + (lr * pTable[row][i] * (pTable[row][z] - pTable[row][y]))
+  print(pTable[row])
 
