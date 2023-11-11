1c202df61a9b5f75d4e0c206ba989d588c2f1b1c
Diego Villamil <dmvillamil@up.edu.ph>
2023-11-11 19:39:01 +0800

    python float addition adds random values

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ lr = float(fp.readline()) #Learning Rate
 th = float(fp.readline()) #Threshold
 bias = float(fp.readline()) 
 numX = 0
+Converging = False
 
 print("Learning Rate: ",lr)
 print("Threshold: ",th)
@@ -33,18 +34,43 @@ ln = len(pTable[0])
 a = ln - 3
 y = ln - 2
 z = ln - 1
-for row in range(0,len(pTable) - 1):
-  #Compute perceptron value a
+
+while(not Converging and (len(iterations) < 1000)):
+  print("Iteration: ", len(iterations) + 1)
+  Converging = True
+  for row in range(0,len(pTable) - 1):
+    #Compute perceptron value a
+    for i in range(0,(numX + 1)):
+      feat = pTable[row][i]
+      weight = pTable[row][i + numX + 1]
+      aVal = feat * weight
+      pTable[row][a] = round(pTable[row][a] + aVal,2)
+
+    #Determine classification
+    pTable[row][y] = 1 if pTable[row][a] > th else 0
+
+    #Adjust weights
+    for i in range(0,(numX + 1)):
+      currW = pTable[row][i + numX + 1]
+      inpVal = pTable[row][i]
+      expected = pTable[row][z]
+      predicted = pTable[row][y]
+      newW = currW + (lr * inpVal * (expected - predicted))
+      pTable[row + 1][i + numX + 1] = round(newW,2)
+    print(pTable[row])
+
+  print(pTable[len(pTable) - 1])
+  print()
+  iterations.append(pTable.copy())
+
+  #Check for convergence
+  for row in range(1,len(pTable) - 1):
+    for i in range(0,(numX + 1)):
+      if(pTable[row][i + numX + 1] != pTable[row + 1][i + numX + 1]):
+        Converging = False
+        break
+    if(not Converging):
+      break
+
   for i in range(0,(numX + 1)):
-    pTable[row][a] = pTable[row][a] + (pTable[row][i] * pTable[row][i + numX + 1])
-
-  #Determine classification
-  pTable[row][y] = 1 if pTable[row][a] >= th else 0
-
-  #Adjust weights
-  for i in range(0,(numX + 1)):
-    currW = pTable[row][i + numX + 1]
-    newW = currW + (lr * pTable[row][i] * (pTable[row][z] - pTable[row][y]))
-    pTable[row + 1][i + numX + 1] = newW
-  print(pTable[row])
-
+    pTable[0][i + numX + 1] = pTable[len(pTable) - 1][i + numX + 1]
